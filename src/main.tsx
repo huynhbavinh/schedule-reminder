7e7414054bc098030b4276945902ffe3cc4f6655
huỳnh bá vinh <55732546+huynhbavinh@users.noreply.github.com>
2023-10-30 21:14:39 +0700

    add boostrap

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
